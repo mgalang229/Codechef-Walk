4fc9abcd6115bad30c1dbf6a627928caee6a0700
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-19 11:37:15 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			int w;
+			cin >> w;
+			// compare current maximum to the current 'w' + 'i'
+			// we add the current index 'i' to the current value of 'w'
+			// in order to find out if the maximum that we've chosen will
+			// be able to reach the last element without failing the conditions
+			// if the current maximum fails the conditions until the end, then
+			// it means that the minimum velocity that we need is the last element
+			// plus the its index (the number is needed to be added to it in order
+			// to pass all the needed conditions)
+			res = max(res, w + i);
+		}
+		cout << res << '\n';
+	}
+	return 0;
+}
